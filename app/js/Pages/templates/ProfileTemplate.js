16848a8e36e3a2f177b88d0e1313dfcb05d4a79c
Klauss <euandrei@live.com>
2017-11-07 19:00:22 +0000

    Add profile template (add missing file in the previous commit)

---
+++ app/js/Pages/templates/ProfileTemplate.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    Text,
+    Image,
+    View,
+    Platform,
+    ScrollView,
+    Dimensions
+} from 'react-native';
+
+import Header from "../../Pages/components/Header";
+import GridSection from "../../Pages/components/GridSection";
+import GridCellWrapper from "../../Pages/components/GridCellWrapper";
+
+import SmallTabbar from "../../Pages/components/SmallTabbar";
+import Tab from "../../Pages/components/Tab";
+
+import NoItem from "../../Pages/components/NoItem";
+
+import UserProfileHeader from "../../Pages/components/UserProfileHeader";
+
+import { colors, storeTemplateStyle } from "../../Pages/styles";
+
+export default class ProfileTemplate extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			activeTab: 0,
+			activeBigTab: 0,
+		}
+		this._renderBuffsTab = this._renderBuffsTab.bind(this);
+		this._renderNerfsTab = this._renderNerfsTab.bind(this);
+	}
+	_renderBuffsTab(){
+		return (
+			<View>
+				<GridSection
+					title={"Title"}
+					titlePosition={"center"}
+					line
+				>
+					{[1,1,1].map((value, i) => {
+						return (
+							<GridCellWrapper
+								key={i}
+								bgColor={colors.watermelonBg}
+								title={"Test"}
+								image={require('../../../assets/nerfs/bear_trap.png')}
+								type={"buff"}
+							/>
+						)
+					})}
+				</GridSection>
+
+				<View style={{marginBottom: 10}}>
+					<GridSection
+						title={"Title"}
+						titlePosition={"center"}
+						line
+
+						empty
+					>
+						<NoItem 
+							message={"No buffs availablle…"}
+							buttonText={"SHOP"}
+							onPress={()=>{}}
+						/>
+					</GridSection>
+				</View>
+
+				<GridSection
+					title={"Title"}
+					titlePosition={"center"}
+					line
+				>
+					{[1,1,1].map((value, i) => {
+						return (
+							<GridCellWrapper
+								key={i}
+								bgColor={colors.watermelonBg}
+								title={"Test"}
+								image={require('../../../assets/nerfs/bear_trap.png')}
+								type={"buff"}
+							/>
+						)
+					})}
+				</GridSection>
+
+			</View>
+		)
+	}
+	_renderNerfsTab(){
+		return (
+			<GridSection
+				title={"Title"}
+				titlePosition={"center"}
+				line
+			>
+				{[1,1,1].map((value, i) => {
+					return (
+						<GridCellWrapper
+							key={i}
+							bgColor={colors.watermelonBg}
+							title={"Test"}
+							image={require('../../../assets/nerfs/bear_trap.png')}
+							type={"buff"}
+						/>
+					)
+				})}
+			</GridSection>
+		)
+	}
+
+	render(){
+		let height = Dimensions.get('window').height;
+
+		return(
+			<ScrollView style={[storeTemplateStyle.container,{minHeight: height}]}>
+				<View style={storeTemplateStyle.header}>
+					<Header 
+						leftIcon={require('../../../assets/back.png')}
+						// logo={require('../../../assets/logo.png')}
+						title={"store"}
+						// circle={"4"}
+					/>
+				</View>
+
+				<View style={storeTemplateStyle.carousel}>
+					<UserProfileHeader
+						wins={'2,478'}
+						followers={'2,478'}
+						following={'2,478'}
+
+						button1={"CHALLENGE"}
+						onButton1={()=>{}}
+
+						graph
+						actualLevel={'7629'}
+						totalLevel={'10000'}
+						user={'ROOKIE'}
+						gold={'34.6K'}
+						onAdd={()=>{}}
+
+						button2={"UNFOLLOW"}
+						onButton2={()=>{}}
+						//button2={"FOLLOW"}
+						>
+					</UserProfileHeader>
+				</View>
+
+				<View style={{paddingLeft: 10, paddingRight: 10}}>
+					<SmallTabbar
+						activeTab={this.state.activeTab}
+						onChange={(id)=>{this.setState({activeTab: id})}}
+						// filter={require('../../../assets/filter.png')}
+						// onFilter={()=>{}}
+					>	
+
+						<Tab 
+							tabTitle={"BUFFS"}
+						>
+							{this._renderBuffsTab()}
+						</Tab>	
+
+						<Tab 
+							tabTitle={"NERFS"}
+						>
+							{this._renderNerfsTab()}
+						</Tab>	
+
+						
+										
+					</SmallTabbar>
+				</View>
+				
+			</ScrollView>
+		)
+	}
+}
