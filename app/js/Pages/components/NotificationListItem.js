a4d8338dee0d750449c051605ed60de8c81c8809
Klauss <euandrei@live.com>
2017-11-07 16:50:15 +0000

    Add more types of notifications

--- app/js/Pages/components/NotificationListItem.js
+++ app/js/Pages/components/NotificationListItem.js
@@ -8,6 +8,7 @@ import {
 
 import GridCellWrapper from "./GridCellWrapper.js"
 import GridSection from "./GridSection.js"
+import TiltButton from "./TiltButton.js"
 
 import { notificationListItemStyle, colors } from "../styles";
 
@@ -17,6 +18,12 @@ TODO replace hardcoded data ie: avatar, arrow cells, grid cells
 
 <NotificationListItem 
   team=[] <- at the moment bool for demo
+
+  arrowed <- bool for now
+
+  buttons <= bool for now
+
+  specialOffer bool
 
   title1={'Text 1 start'}
   text1={'finishing ...'}
@@ -33,126 +40,224 @@ export default class NotificationListItem extends React.Component {
 		this.state = {
 		}
 	}
+
+  _renderTeam() {
+    return (
+      <View>
+        <GridSection
+          // title={"Title"}
+          // titlePosition={"center"}
+          // lines
+        >
+          <GridCellWrapper
+            bgColor={colors.watermelonBg}
+            title={"Test"}
+            image={require('../../../assets/nerfs/bear_trap.png')}
+            type={"power-up"}
+          />
+          
+          <GridCellWrapper
+            bgColor={colors.greenishTurquoiseBg}
+            title={"Test"}
+            image={require('../../../assets/nerfs/bear_trap.png')}
+            type={"power-up"}
+          />
+          <GridCellWrapper
+            bgColor={colors.greenishTurquoiseBg}
+            title={"Test"}
+            image={require('../../../assets/buffs/cannon.png')}
+            type={"power-up"}
+          />
+
+          <GridCellWrapper
+            bgColor={colors.watermelonBg}
+            title={"Test"}
+            image={require('../../../assets/nerfs/bear_trap.png')}
+            type={"power-up"}
+          />
+          
+          <GridCellWrapper
+            bgColor={colors.greenishTurquoiseBg}
+            title={"Test"}
+            image={require('../../../assets/nerfs/bear_trap.png')}
+            type={"power-up"}
+          />
+          <GridCellWrapper
+            bgColor={colors.greenishTurquoiseBg}
+            title={"Test"}
+            image={require('../../../assets/buffs/cannon.png')}
+            type={"power-up"}
+          />
+
+          <GridCellWrapper
+            bgColor={colors.watermelonBg}
+            title={"Test"}
+            image={require('../../../assets/nerfs/bear_trap.png')}
+            type={"power-up"}
+          />
+          
+          <GridCellWrapper
+            bgColor={colors.greenishTurquoiseBg}
+            title={"Test"}
+            image={require('../../../assets/nerfs/bear_trap.png')}
+            type={"power-up"}
+          />
+          <GridCellWrapper
+            bgColor={colors.greenishTurquoiseBg}
+            title={"Test"}
+            image={require('../../../assets/buffs/cannon.png')}
+            type={"power-up"}
+          />
+        </GridSection>
+      </View>
+    )
+  }
+
+  _renderArrowed() {
+    return (
+      <View style={notificationListItemStyle.arrowNotificationContainer}>
+        <View style={notificationListItemStyle.arrowNotificationWrapper}>
+          <GridCellWrapper
+            bgColor={colors.watermelonBg}
+            title={"Test"}
+            image={require('../../../assets/placeholder.png')}
+            type={"player"}
+          />
+
+          <View style={notificationListItemStyle.arrowImageWrapper}>
+            <Image 
+              style={notificationListItemStyle.arrowImage}
+              source={{uri: 'http://via.placeholder.com/21x16'}}
+            />
+          </View>
+
+          <GridCellWrapper
+            bgColor={colors.watermelonBg}
+            title={"Test"}
+            image={require('../../../assets/placeholder.png')}
+            type={"power-up"}
+          />
+        </View>
+      </View>
+    )
+  }
+
+  _renderButtons(){
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flex: 1, flexDirection: 'row', width: 220}}>
+          <TiltButton 
+            text={"DECLINE"}
+            borderColor={colors.sunnyYellow}
+            backgroundColor={colors.sunnyYellowBg}
+            onPress={()=>{}}
+
+            style={{marginRight: 3}}
+          />
+
+          <TiltButton 
+            text={"ACCEPT"}
+            borderColor={colors.sunnyYellow}
+            backgroundColor={colors.sunnyYellowBg}
+            onPress={()=>{}}
+
+            style={{marginLeft: 3}}
+          />
+        </View>
+      </View>
+    )
+  }
+
+  _renderSpecialOffer(){
+    return (
+      <View style={notificationListItemStyle.specialOfferContainer}>
+
+        <View style={notificationListItemStyle.cellComponentsWrapper}>
+          <View style={notificationListItemStyle.cellWrapper}>
+            <GridCellWrapper
+              bgColor={colors.watermelonBg}
+              title={"Test"}
+              image={require('../../../assets/placeholder.png')}
+              type={"player"}
+            />
+          </View>
+
+          <Text style={notificationListItemStyle.actualPriceLabel}>
+              $50.00
+          </Text>
+
+          <Text style={notificationListItemStyle.discountPriceLabel}>
+            $50.00
+          </Text>
+        </View>
+
+        <View style={notificationListItemStyle.buttonsContainer}>
+          <TiltButton 
+            text={"DECLINE"}
+            borderColor={colors.sunnyYellow}
+            backgroundColor={colors.sunnyYellowBg}
+            onPress={()=>{}}
+
+            style={{marginRight: 3}}
+          />
+
+          <TiltButton 
+            text={"ACCEPT"}
+            borderColor={colors.sunnyYellow}
+            backgroundColor={colors.sunnyYellowBg}
+            onPress={()=>{}}
+
+            style={{marginLeft: 3}}
+          />
+        </View>
+      </View>
+    )
+  }
 	
 	render(){
 
 		return (
-			<View style={notificationListItemStyle.container}>
+			<View style={this.props.arrowed || this.props.team ? notificationListItemStyle.container : notificationListItemStyle.containerMinimal}>
 
         <View style={notificationListItemStyle.notificationHeader}>
 
           <Image source={require('../../../assets/placeholder.png')} style={notificationListItemStyle.avatar} />
           
           <View style={notificationListItemStyle.headerTitleWrapper}>
-            <Text style={notificationListItemStyle.headerTitlePre}>
-              <Text style={notificationListItemStyle.headerTitlePost}>{this.props.title1}</Text>
-              {' '}
-              {this.props.text1}
-            </Text>
-            <Text style={notificationListItemStyle.headerTitlePre}>
-              <Text style={notificationListItemStyle.headerTitlePost}>{this.props.title2}</Text>
-              {' '}
-              {this.props.text2}
-            </Text>
+
+            {this.props.text1 && (
+              <Text style={notificationListItemStyle.headerTitlePre}>
+                <Text style={notificationListItemStyle.headerTitlePost}>{this.props.title1}</Text>
+                {' '}
+                {this.props.text1}
+              </Text>
+            )}
+
+            {this.props.text2 && (
+              <Text style={notificationListItemStyle.headerTitlePre}>
+                <Text style={notificationListItemStyle.headerTitlePost}>{this.props.title2}</Text>
+                {' '}
+                {this.props.text2}
+              </Text>
+            )}
+
+            {this.props.specialOffer && (
+              <View style={notificationListItemStyle.specialOfferHeaderWrapper}>
+                <Text style={notificationListItemStyle.specialOfferHeaderTitle}>Special Offer</Text>
+                <View style={notificationListItemStyle.specialOfferHeaderLine}></View>
+              </View>
+            )}
           </View>
         </View>
 
-        {this.props.team ? (
-          <View>
-            <GridSection
-              // title={"Title"}
-              // titlePosition={"center"}
-              // lines
-            >
-              <GridCellWrapper
-                bgColor={colors.watermelonBg}
-                title={"Test"}
-                image={require('../../../assets/nerfs/bear_trap.png')}
-                type={"power-up"}
-              />
-              
-              <GridCellWrapper
-                bgColor={colors.greenishTurquoiseBg}
-                title={"Test"}
-                image={require('../../../assets/nerfs/bear_trap.png')}
-                type={"power-up"}
-              />
-              <GridCellWrapper
-                bgColor={colors.greenishTurquoiseBg}
-                title={"Test"}
-                image={require('../../../assets/buffs/cannon.png')}
-                type={"power-up"}
-              />
+        {this.props.team && this._renderTeam()}
 
-              <GridCellWrapper
-                bgColor={colors.watermelonBg}
-                title={"Test"}
-                image={require('../../../assets/nerfs/bear_trap.png')}
-                type={"power-up"}
-              />
-              
-              <GridCellWrapper
-                bgColor={colors.greenishTurquoiseBg}
-                title={"Test"}
-                image={require('../../../assets/nerfs/bear_trap.png')}
-                type={"power-up"}
-              />
-              <GridCellWrapper
-                bgColor={colors.greenishTurquoiseBg}
-                title={"Test"}
-                image={require('../../../assets/buffs/cannon.png')}
-                type={"power-up"}
-              />
+        {this.props.arrowed && this._renderArrowed()}
 
-              <GridCellWrapper
-                bgColor={colors.watermelonBg}
-                title={"Test"}
-                image={require('../../../assets/nerfs/bear_trap.png')}
-                type={"power-up"}
-              />
-              
-              <GridCellWrapper
-                bgColor={colors.greenishTurquoiseBg}
-                title={"Test"}
-                image={require('../../../assets/nerfs/bear_trap.png')}
-                type={"power-up"}
-              />
-              <GridCellWrapper
-                bgColor={colors.greenishTurquoiseBg}
-                title={"Test"}
-                image={require('../../../assets/buffs/cannon.png')}
-                type={"power-up"}
-              />
-            </GridSection>
-          </View>
-        ) : (
-          <View style={notificationListItemStyle.arrowNotificationContainer}>
-            <View style={notificationListItemStyle.arrowNotificationWrapper}>
-              <GridCellWrapper
-                bgColor={colors.watermelonBg}
-                title={"Test"}
-                image={require('../../../assets/placeholder.png')}
-                type={"player"}
-              />
+        {this.props.buttons && this._renderButtons()}
 
-              <View style={notificationListItemStyle.arrowImageWrapper}>
-                <Image 
-                  style={notificationListItemStyle.arrowImage}
-                  source={{uri: 'http://via.placeholder.com/21x16'}}
-                />
-              </View>
-
-              <GridCellWrapper
-                bgColor={colors.watermelonBg}
-                title={"Test"}
-                image={require('../../../assets/placeholder.png')}
-                type={"power-up"}
-              />
-            </View>
-          </View>
-        )}
-
-
+        {this.props.specialOffer && this._renderSpecialOffer()}
+        
       </View>
 		)
 	}
